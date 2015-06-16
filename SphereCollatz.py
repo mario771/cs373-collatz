93e4c0f429af2ee8e6bf8913a0923465a83cde1e
Mario <mario771@cs.utexas.edu>
2015-06-15 19:08:01 -0500

    removed some files

--- SphereCollatz.py
+++ SphereCollatz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-# ---------------------------
-# projects/collatz/Collatz.py
-# Copyright (C) 2015
-# Glenn P. Downing
-# ---------------------------
+# -------
+# imports
+# -------
 
-lazy_cache = {} # dictionary used for storing cycle lengths
-
+import sys
+lazy_cache = {} #dictionary used for storing cycle lengths
 # ------------
 # collatz_read
 # ------------
@@ -35,7 +33,7 @@ def collatz_eval (i, j) :
     assert i < 999999
     assert j > 0
     assert j < 999999
-    
+
     max_cycles = 0
     cycles = 0
     m = 0
@@ -46,14 +44,14 @@ def collatz_eval (i, j) :
         start = j
         stop = i
     if start == 1 and stop == 1:
-        return 1       
+        return 1
     if start == stop:
         return compute_cycles(start)
 
     m = (stop // 2) + 1 #range optimization studied in class
     if start < m:
         start = m
-    
+
     for i in range(start,stop + 1):
         if i in lazy_cache:
             cycles = lazy_cache[i]
@@ -71,14 +69,14 @@ def compute_cycles(num):
     assert num > 0
     cycles = 1
     original_num = num
-    cached_val = 0 
+    cached_val = 0
 
     while num != 1:
         if num in lazy_cache:
             cached_val = (lazy_cache[num] + cycles) - 1
             lazy_cache[original_num] = cached_val
             assert cached_val > 0 
-            return cached_val        
+            return cached_val
         else:
             if num % 2 == 0:
                 num  = num >> 1
@@ -118,3 +116,35 @@ def collatz_solve (r, w) :
         i, j = collatz_read(s)
         v    = collatz_eval(i, j)
         collatz_print(w, i, j, v)
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    collatz_solve(sys.stdin, sys.stdout)
+
+"""
+% cat RunCollatz.in
+1 10
+100 200
+201 210
+900 1000
+
+
+
+% RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+
+
+% cat RunCollatz.out
+1 10 1
+100 200 1
+201 210 1
+900 1000 1
+
+
+
+% pydoc3 -w Collatz
+# That creates the file Collatz.html
+"""
